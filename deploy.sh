a66f04371737f2a9fae414c3efa58868888a12fd
Saikat Chakrabortty <saikatchakrabortty2@gmail.com>
2021-01-31 19:00:28 +0530

    Renaming

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,14 @@ if [ "${NAMESPACE}" = "" ]; then
 fi
 
 # do the actual upgrade via helm
-helm upgrade "${NAMESPACE}-sample-app" ./helming \
+helm upgrade "${NAMESPACE}-sample-app" ./chart \
     --namespace="${NAMESPACE}" $VALUES \
     --install
 
 
 echo """
     Try this as well:
-        helm upgrade ${NAMESPACE}-sample-app-custom ./helming --namespace="${NAMESPACE}" --values values-gen.yaml --install
+        helm upgrade ${NAMESPACE}-sample-app-custom ./chart --namespace="${NAMESPACE}" --values values-gen.yaml --install
 
         If you get Error, Make sure to run the uninstall/delete command, 
         (The Error is due to the way I have architected , it prevents the same chart to overright others)
